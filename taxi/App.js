e0f26f9b9d18d5dcab3f53959b452346f5f43ccd
Gyubin-Lee <leegyubin00@gmail.com>
2020-03-08 16:22:31 +0900

    Taxi v.0.0.2

--- taxi/App.js
+++ taxi/App.js
@@ -15,11 +15,14 @@ import {
 
 import store from './store.js';
 
+import Room from './components/Room';
+
 class App extends Component{
   render(){
     return(
       <View>
         <Text>카택전에 오신것을 환영합니다</Text>
+        <Room></Room>
       </View>
     );
   }
