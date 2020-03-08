34082f76db6512541a0b561a6b17feb2d7bb0acd
Gyubin-Lee <leegyubin00@gmail.com>
2020-03-08 14:55:13 +0900

    Taxi v0.0

---
+++ taxi/App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+import store from './store.js';
+
+class App extends Component{
+  render(){
+    return(
+      <View>
+        <Text>카택전</Text>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
---
+++ taxi/store.js
@@ -0,0 +1,9 @@
+import {createStore} from 'redux';
+
+export default createStore(function(state, action){
+    if(state === undefined){
+        return {mode: "MAIN"}
+    }
+    return state;
+}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
