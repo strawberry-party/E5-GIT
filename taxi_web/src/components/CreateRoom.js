86f9b181348830f6523f1620a318d73d717ab6a3
sangachoi1018 <sangachoi2103@gmail.com>
2020-03-19 20:35:42 +0900

    뭔가 이상한 버전

--- taxi_web/src/components/CreateRoom.js
+++ taxi_web/src/components/CreateRoom.js
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom'
 
 class CreateRoom extends Component {
     render() {
-        var writer, dep, dest, desc;
+        var writer, dep, dest, mnum, desc;
         return (
             <MuiThemeProvider>
                 <TextField floatingLabelText="작성자" onChange={function (e, text) {
@@ -21,15 +21,18 @@ class CreateRoom extends Component {
                 <TextField floatingLabelText="추가정보" onChange={function (e, text) {
                     desc = text
                 }} /><br />
+                <TextField floatingLabelText="최대인원" onChange={function (e, text) {
+                    mnum = text
+                }} /><br />
                 <NavLink exact to={'/'}>
                     <RaisedButton label="방 개설" onClick={function () {
-                        if (writer !== undefined && dep !== undefined && dest !== undefined) {
-                            if(desc === undefined){
+                        if (writer !== undefined && dep !== undefined && dest !== undefined && mnum !== undefined) {
+                            if (desc === undefined) {
                                 desc = '추가정보 없음';
                             }
-                            this.props.onClick('create_process', writer, dep, dest, desc);
+                            this.props.onClick('create', writer, dep, dest, desc);
                         } else {
-                            window.alert('다시 입력하세요!');    
+                            window.alert('다시 입력하세요!');
                         }
                     }.bind(this)} />
                 </NavLink>
@@ -42,8 +45,8 @@ export default connect(
     null,
     function (dispatch) {
         return {
-            onClick: function (mode, writer, dep, dest, desc) {
-                dispatch({ type: mode, writer, dep, dest, desc })
+            onClick: function (mode, writer, dep, dest, mnum) {
+                dispatch({ type: mode, writer, dep, dest, mnum })
             }
         }
     }
--- taxi_web/src/store.js
+++ taxi_web/src/store.js
@@ -15,53 +15,65 @@ var initstate = {
     notices: [
         { id: 1, title: '중요공지', desc: '카택전 v0.1 런칭' }
     ]
+}
+var myRooms = [];
+    myRooms = rawInitState.rooms.filter(function (room) {
+        if (room.userId === rawInitState.userId) {
+            console.log("yes!");
+            return true;
+        }
+        return false;
+    });
 
+    var initstate = { ...rawInitState, myRooms };
+
+    function reducer(state = initstate, action) {
+        var newState, newMyRooms;
+if (action.type === 'create_process') {
+    var currentMaxId = state.rooms.length;
+    var newId = currentMaxId + 1;
+    var room = { id: newId, writer: action.writer, dep: action.dep, dest: action.dest, desc: action.desc, joinedUsers: [state.userId], maxNum: Number(action.mnum) };
+    var newRooms = [...state.rooms, room];
+    var newMyRooms = [...state.myRooms, room]
+    newState = { ...state, mode: 'home', rooms: newRooms, maxId: newId, myRooms: newMyRooms };
+    return newState;
+}
+if (action.type === 'addMyRoom') {
+    var room = state.rooms[action.roomId - 1];
+    newMyRooms = [...state.myRooms, room];
+    newState = { ...state, myRooms: newMyRooms };
+    return newState;
 }
 
-function reducer(state = initstate, action) {
-    var newState, newMyRooms;
-    if (action.type === 'create_process') {
-        var currentMaxId = state.rooms.length;
-        var newId = currentMaxId + 1;
-        var newRooms = [...state.rooms, { id: newId, writer: action.writer, dep: action.dep, dest: action.dest, desc: action.desc }];
-        newState = { ...state, mode: 'home', rooms: newRooms, maxId: newId };
-        return newState;
-    }
-    if (action.type === 'addMyRoom') {
-        var room = state.rooms[action.roomId - 1];
-        newMyRooms = [...state.myRooms, room];
-        newState = { ...state, myRooms: newMyRooms };
-        return newState;
-    }
-    
-    if (action.type === 'deleteRoom') {
-        newRooms = state.rooms.filter(function(room){
-            if(room.id === action.roomId){
-                return false;
-            }
-            return true;
-        });
-        newMyRooms = state.myRooms.filter(function(myRoom){
-            if(myRoom.id === action.roomId){
-                return false;
-            }
-            return true;
-        });
+if (action.type === 'deleteRoom') {
+    newRooms = state.rooms.filter(function (room) {
+        if (room.id === action.roomId) {
+            return false;
+        }
+        return true;
+    });
+    newMyRooms = state.myRooms.filter(function (myRoom) {
+        if (myRoom.id === action.roomId) {
+            return false;
+        }
+        return true;
+    });
 
-        newState = { ...state, rooms: newRooms, myRooms: newMyRooms };
-        return newState;
-    }
-    if (action.type === 'deleteMyRoom') {
-        newMyRooms = state.myRooms.filter(function(myRoom){
-            if(myRoom.id === action.roomId){
-                return false;
-            }
-            return true;
-        });
-        newState = { ...state, myRooms: newMyRooms };
-        return newState;
-    }
-    return state;
+    newState = { ...state, rooms: newRooms, myRooms: newMyRooms };
+    return newState;
+}
+if (action.type === 'deleteMyRoom') {
+    newMyRooms = state.myRooms.filter(function (myRoom) {
+        if (myRoom.id === action.roomId) {
+            return false;
+        }
+        return true;
+    });
+    newState = { ...state, myRooms: newMyRooms };
+    newState.rooms.num = newState.rooms.num - 1
+    return newState;
+}
+return state;
 }
 
 export default createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
